ebab6c7c08e39258a4649545c82f025bb8cd1ae2
Ravi Ranjan Pandey <raviranjanpandey@outlook.com>
2021-01-11 22:57:20 +0530

    dotnet 5.0 migrations from dotnet core 3.1

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,9 +1,16 @@
-using IOC.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -18,9 +25,13 @@ namespace API
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {         
+        {
+
             services.AddControllers();
-            ConfigureServicesExtensions.ConfigureServicesInAssembly(services, Configuration);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +40,8 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
             app.UseHttpsRedirection();
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Eventful is an Event Manager Web Application where user can create, host or join an event. User can create an event group and can follow and chat to each other.
-The Project is a combination of .net core 3.1 Webapi and .net core technologies for the backend and React JS for the frontend. It has utilised best coding practices, architecture, patterns and clean coding principles.
+The Project is a combination of dotnet 5.0 Webapi and dotnet technologies for the backend and React JS for the frontend. It has utilised best coding practices, architecture, patterns and clean coding principles.
 
 Here is the summary of the project:
 1. The Web API is built in .Net Core with Clean Architecture using the CQRS + Mediator pattern
 2. It explains how to use AutoMapper and MediatR packages in the .Net projects
-3. Its a multi-project solution with .Net Core
+3. Its a multi-project solution with latest .net framework dotnet 5.0
 4. It has Entity Framework Core as the Object Relational Mapper
 5. It also explains how to integrate ASPNET Core SignalR into an application for real time web communication
 6. Identity and Authentication is done using .Net Core Identity

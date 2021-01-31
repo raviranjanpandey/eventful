20aa07c4a90e585cb7e7d9677b431aeb880aafdf
raviranjanpandey <raviranjanpandey@outlook.com>
2021-01-31 17:16:12 +0530

    API Integrated

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component, useState, useEffect } from 'react';
+import axios from 'axios';
+import {Header, HeaderContent, Icon, List} from 'semantic-ui-react';
+import {IActivity} from '../models/activity';
 
-const App: React.FC = () =>{
+
+function App()
+{
+  const [activities,setActivity] = useState<IActivity[]>([]);
+
+  useEffect(() =>
+  {
+    axios
+    .get<IActivity[]>('https://localhost:5001/api/Events/GetAll')
+    .then(response =>
+      {
+        setActivity(response.data)
+      }
+      );
+  },[]);
+  
   return (
     <div>
-    <h1>Eventful</h1>
+    <Header as = 'h2'>
+      <Icon name = 'users' />
+      <Header.Content>Events</Header.Content>
+      </Header>
+      <List>
+        {
+          activities.map(activity => {
+            <List.Item key = {activity.id}>{activity.title}</List.Item>
+          })
+        }
+      </List>
     </div>
   );
 }
---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,10 @@
+export interface IActivity
+{
+    id : string,
+    title : string,
+    description : string,
+    category : string,
+    date : Date,
+    city : string,
+    venue : string
+}

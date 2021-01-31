141598f088c31584ee2322134d85e19633be3eef
raviranjanpandey <raviranjanpandey@outlook.com>
2021-01-31 17:34:15 +0530

    Cors Policy added

---
+++ API/Extensions/Configurations/CorsConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions.Configurations
+{
+    public class CorsConfig : IConfigurable
+    {
+        public void ConfigureService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithOrigins(new string[] { "http://localhost:3000", "test" });
+                });
+            });
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,8 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

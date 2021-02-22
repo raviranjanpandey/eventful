10f9c4c73a3e0fe3ddb40d5ac9c2f5143c77f378
raviranjanpandey <raviranjanpandey@outlook.com>
2021-02-22 10:24:03 +0530

    added mobx and refactored loading list

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,36 @@
+import { makeAutoObservable } from "mobx";
+import agent from "../api/agent";
+import { Activity } from "../models/activity";
+
+export default class ActivityStore {
+
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode = false;
+    loading = false;
+    loadingInitial = false;
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    loadActivities = async () => {
+        this.setLoadingInitial(true);
+        try {
+            const activitylist = await agent.Activities.list();
+            activitylist.forEach((activity) => {
+                activity.date = activity.date.split("T")[0];
+                this.activities.push(activity);
+              });
+              this.setLoadingInitial(false);
+        } catch (error) {
+            console.log(error);
+            this.setLoadingInitial(false);
+        }
+    }
+
+    setLoadingInitial = (state: boolean) =>{
+        this.loadingInitial = state;
+    }
+
+}
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app/layout/App';
-import reportWebVitals from './reportWebVitals';
-import 'semantic-ui-css/semantic.min.css';
-import './app/layout/styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/layout/App";
+import reportWebVitals from "./reportWebVitals";
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+import { store, StoreContext } from "./app/stores/store";
+
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

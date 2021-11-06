d121e751f43e66873bc385e5eea111b4ff9355ba
raviranjanpandey <raviranjanpandey@outlook.com>
2021-11-06 13:02:08 +0530

    Pagination Implementation Client Side

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { Activity, ActivityFormValues } from '../models/activity';
+import { PaginatedResult } from '../models/pagination';
 import { Photo, Profile } from '../models/profile';
 import { User, UserFormValues } from '../models/user';
 import { store } from '../stores/store';
@@ -22,6 +23,12 @@ axios.interceptors.request.use(config => {
 
 axios.interceptors.response.use(async response => {
     await sleep(1000);
+    const pagination = response.headers['pagination'];
+    if(pagination)
+    {
+        response.data = new PaginatedResult(response.data, JSON.parse(pagination));
+        return response as AxiosResponse<PaginatedResult<any>>
+    }
     return response;
 }, (error: AxiosError) => {
     const {data, status, config} = error.response!;
@@ -66,7 +73,8 @@ const requests = {
 }
 
 const Activities = {
-    list: () => requests.get<Activity[]>("/Events/GetAll"),
+    list: (params: URLSearchParams) => axios.get<PaginatedResult<Activity[]>>("/Events/GetAll",{params})
+    .then(responseBody),
     details: (id: string) => requests.get<Activity>(`/Events/GetById/${id}`),
     create: (activity: ActivityFormValues) =>
       requests.post<void>("/Events/Create", activity),
--- client-app/src/features/events/dashboard/EventDashboard.tsx
+++ client-app/src/features/events/dashboard/EventDashboard.tsx
@@ -1,28 +1,48 @@
 import { observer } from "mobx-react-lite";
-import React, { useEffect } from "react";
-import { Grid } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroller";
+import { Grid, Loader } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { PagingParams } from "../../../app/models/pagination";
 import { useStore } from "../../../app/stores/store";
 import EventFilters from "./EventFilters";
 import EventList from "./EventList";
 
 export default observer(function EventDashboard() {
   const { activityStore } = useStore();
-  const { loadActivities, activityRegistry } = activityStore;
+  const { loadActivities, activityRegistry, setPagingParams, pagination } = activityStore;
+  const [loadingNext, setLoadingNext] = useState(false);
+
+  function handleGetNext() {
+    setLoadingNext(true);
+    setPagingParams(new PagingParams(pagination!.currentPage + 1))
+    loadActivities().then(() => setLoadingNext(false));
+  }
 
   useEffect(() => {
-    if(activityRegistry.size <= 1) loadActivities();
-  }, [activityRegistry.size,loadActivities]);
+    if (activityRegistry.size <= 1) loadActivities();
+  }, [activityRegistry.size, loadActivities]);
 
-  if (activityStore.loadingInitial)
+  if (activityStore.loadingInitial && !loadingNext)
     return <LoadingComponent content="Loading events..." />;
   return (
     <Grid>
       <Grid.Column width="10">
-        <EventList />
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={handleGetNext}
+          hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+          initialLoad={false}
+        >
+          <EventList />
+        </InfiniteScroll>
+
       </Grid.Column>
       <Grid.Column width="6">
-       <EventFilters />
+        <EventFilters />
+      </Grid.Column>
+      <Grid.Column width = {10}>
+      <Loader active = {loadingNext}/>
       </Grid.Column>
     </Grid>
   );
